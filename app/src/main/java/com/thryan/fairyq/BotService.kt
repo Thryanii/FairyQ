4c86c777d6b4ee3d6ba451f0e79856ae206aef1d
thryanii <thryanii@outlook.com>
2022-10-23 20:13:41 +0800

    Initial commit

---
+++ app/src/main/java/com/thryan/fairyq/BotService.kt
@@ -0,0 +1,12 @@
+package com.thryan.fairyq
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class BotService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
